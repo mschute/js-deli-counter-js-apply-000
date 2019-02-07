5f180bd6147a1512eef0f27995240715ee40c90d
Marta U <marta.unterschute@gmail.com>
2019-02-07 17:33:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, newPersonName){
+ katzDeliLine.push(newPersonName);
+  return `Welcome, ${newPersonName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    var serving = katzDeliLine.shift();
+      return `Currently serving ${serving}.`;
+  } else {return 'There is nobody waiting to be served!'}
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return 'The line is currently empty.'
+  }
+    
+    var numberName = [];
+    
+  for (let i = 0; i < katzDeliLine.length; i++){
+    numberName.push(`${i + 1}. ${katzDeliLine[i]}`)
+   }
+   return `The line is currently: ${numberName.join(', ')}`
+}
